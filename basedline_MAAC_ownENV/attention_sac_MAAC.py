6639ca36056c91fcc5608c5cec19ea61b3178562
zhangmingcheng28 <zhangmingcheng28@hotmail.com>
2023-07-04 16:58:05 +0800

    added MAAC for my env

---
+++ basedline_MAAC_ownENV/attention_sac_MAAC.py
@@ -0,0 +1,297 @@
+import torch
+import torch.nn.functional as F
+import numpy as np
+
+from torch.optim import Adam
+from utils_MAAC.misc import soft_update, hard_update, enable_gradients, disable_gradients, device
+from utils_MAAC.agents import AttentionAgent
+from utils_MAAC.critics import AttentionCritic
+
+MSELoss = torch.nn.MSELoss()
+
+class AttentionSAC(object):
+    """
+    Wrapper class for SAC agents with central attention critic in multi-agent
+    task
+    """
+    def __init__(self, agent_init_params, sa_size,
+                 gamma=0.95, tau=0.01, pi_lr=0.01, q_lr=0.01,
+                 reward_scale=10.,
+                 pol_hidden_dim=128,
+                 critic_input_dim=None,
+                 critic_hidden_dim=128, attend_heads=4,
+                 **kwargs):
+        """
+        Inputs:
+            agent_init_params (list of dict): List of dicts with parameters to
+                                              initialize each agent
+                num_in_pol (int): Input dimensions to policy
+                num_out_pol (int): Output dimensions to policy
+            sa_size (list of (int, int)): Size of state and action space for
+                                          each agent
+            gamma (float): Discount factor
+            tau (float): Target update rate
+            pi_lr (float): Learning rate for policy
+            q_lr (float): Learning rate for critic
+            reward_scale (float): Scaling for reward (has effect of optimal
+                                  policy entropy)
+            hidden_dim (int): Number of hidden dimensions for networks
+        """
+        self.nagents = len(sa_size)
+
+        self.agents = [AttentionAgent(lr=pi_lr,
+                                      hidden_dim=pol_hidden_dim,
+                                      **params)
+                         for params in agent_init_params]
+        self.critic = AttentionCritic(sa_size, critic_input_dim, hidden_dim=critic_hidden_dim,
+                                      attend_heads=attend_heads)
+        self.target_critic = AttentionCritic(sa_size, critic_input_dim, hidden_dim=critic_hidden_dim,
+                                             attend_heads=attend_heads)
+        hard_update(self.target_critic, self.critic)
+        self.critic_optimizer = Adam(self.critic.parameters(), lr=q_lr,
+                                     weight_decay=1e-3)
+        self.var = [1.0 for _ in range(len(sa_size))]
+        self.agent_init_params = agent_init_params
+        self.gamma = gamma
+        self.tau = tau
+        self.pi_lr = pi_lr
+        self.q_lr = q_lr
+        self.reward_scale = reward_scale
+        self.pol_dev = 'cpu'  # device for policies
+        self.critic_dev = 'cpu'  # device for critics
+        self.trgt_pol_dev = 'cpu'  # device for target policies
+        self.trgt_critic_dev = 'cpu'  # device for target critics
+        self.niter = 0
+
+    @property
+    def policies(self):
+        return [a.policy for a in self.agents]
+
+    @property
+    def target_policies(self):
+        return [a.target_policy for a in self.agents]
+
+    def step(self, state, episode, explore=False):
+        obs = torch.from_numpy(np.stack(state[0])).float().to(device)
+        obs_grid = torch.from_numpy(np.stack(state[1])).float().to(device)
+        all_obs_surAgent = []
+        for each_agent_sur in state[2]:
+            each_obs_surAgent = np.squeeze(np.array(each_agent_sur), axis=1)
+            all_obs_surAgent.append(torch.from_numpy(each_obs_surAgent).float())
+
+        actions = torch.zeros(len(self.agents), self.agent_init_params[0]['num_out_pol'])
+        FloatTensor = torch.cuda.FloatTensor if torch.cuda.is_available() else torch.FloatTensor
+        for i in range(len(self.agents)):
+            sb = obs[i].detach()
+            sb_grid = obs_grid[i].detach()
+            sb_surAgent = all_obs_surAgent[i].detach()
+            act = self.agents[i].step([sb.unsqueeze(0), sb_grid.unsqueeze(0), sb_surAgent.unsqueeze(0)], explore=explore)
+            # if explore:
+            #     act += torch.from_numpy(np.random.randn(2) * self.var[i]).type(FloatTensor)
+            #     self.var[i] = self.get_scaling_factor(episode, 12000)  # self.var[i] will decrease as the episode increase
+            #
+            # act = torch.clamp(act, -1.0, 1.0)
+            #
+            actions[i, :] = act
+        return actions.data.cpu().numpy()
+
+    def get_scaling_factor(self, episode, drop_point, start_scale=1, end_scale=0.03):
+        if episode <= drop_point:
+            slope = (end_scale - start_scale) / drop_point
+            return slope * episode + start_scale
+        else:
+            return end_scale
+
+    def update_critic(self, sample, soft=True, **kwargs):
+        """
+        Update central critic for all agents
+        """
+        obs, acs, rews, next_obs, dones = sample
+        rews = list(rews.transpose(0,1))
+        dones = list(dones.transpose(0,1))
+        all_probs = []
+        all_log_pis = []
+        all_pol_regs = []
+
+        # Q loss
+        next_acs = []
+        next_log_pis = []
+        for pi_idx, pi in enumerate(self.target_policies):
+            curr_next_ac, curr_next_log_pi = pi([next_obs[0][pi_idx], next_obs[1][pi_idx], next_obs[2][pi_idx]],
+                                                       return_log_pi=True)
+            next_acs.append(curr_next_ac)
+            next_log_pis.append(curr_next_log_pi)
+
+        trgt_critic_in = (next_obs, next_acs)
+        next_qs = self.target_critic(trgt_critic_in)  # input is a list, length is total number of agents. Each element holds each agent's next state and action with batch information.
+        acs = list(acs.transpose(0, 1))
+        critic_in = (obs, acs)
+        critic_rets = self.critic(critic_in, regularize=True, niter=self.niter)
+        q_loss = 0
+        for a_i, nq, log_pi, (pq, regs) in zip(range(5), next_qs, next_log_pis, critic_rets):
+            target_q = (rews[a_i].view(-1, 1) + self.gamma * nq * (1 - dones[a_i].view(-1, 1)))
+            if soft:
+                target_q -= log_pi / self.reward_scale  # this reward_scale is set to 100
+            q_loss += MSELoss(pq, target_q.detach())  # summing the MSE loss across all agents
+            for reg in regs:
+                q_loss += reg  # regularizing attention
+        q_loss.backward()
+        self.critic.scale_shared_grads()
+        # grad_norm = torch.nn.utils.clip_grad_norm(
+        #     self.critic.parameters(), 10 * self.nagents)  # originally used for logger
+        self.critic_optimizer.step()
+        self.critic_optimizer.zero_grad()
+
+        self.niter += 1
+
+    def update_policies(self, sample, soft=True, **kwargs):
+        obs, acs, rews, next_obs, dones = sample
+        samp_acs = []
+        all_probs = []
+        all_log_pis = []
+        all_pol_regs = []
+        for pi_idx, pi in enumerate(self.policies):
+            curr_ac, probs, curr_log_pi, pol_regs = pi([obs[0][pi_idx], obs[1][pi_idx], obs[2][pi_idx]],
+                                                       return_all_probs=True, return_log_pi=True, regularize=True)
+            samp_acs.append(curr_ac)
+            all_probs.append(probs)
+            all_log_pis.append(curr_log_pi)
+            all_pol_regs.append(pol_regs)
+
+        # critic_in = list(zip(obs, samp_acs))
+        critic_in = (obs, samp_acs)
+        critic_rets = self.critic(critic_in, return_all_q=True)
+        for a_i, probs, log_pi, pol_regs, (q, all_q) in zip(range(5), all_probs,
+                                                            all_log_pis, all_pol_regs,
+                                                            critic_rets):
+            curr_agent = self.agents[a_i]
+            v = (all_q * probs).sum(dim=1, keepdim=True)
+            pol_target = q - v
+            if soft:
+                pol_loss = (log_pi * (log_pi / self.reward_scale - pol_target).detach()).mean()
+            else:
+                pol_loss = (log_pi * (-pol_target).detach()).mean()
+            for reg in pol_regs:
+                pol_loss += 1e-3 * reg  # policy regularization
+            # don't want critic to accumulate gradients from policy loss
+            disable_gradients(self.critic)
+            pol_loss.backward()
+            enable_gradients(self.critic)
+
+            grad_norm = torch.nn.utils.clip_grad_norm_(curr_agent.policy.parameters(), 0.5)
+            curr_agent.policy_optimizer.step()
+            curr_agent.policy_optimizer.zero_grad()
+
+
+    def update_all_targets(self):
+        """
+        Update all target networks (called after normal updates have been
+        performed for each agent)
+        """
+        soft_update(self.target_critic, self.critic, self.tau)
+        for a in self.agents:
+            soft_update(a.target_policy, a.policy, self.tau)
+
+    def prep_training(self, device='gpu'):
+        self.critic.train()
+        self.target_critic.train()
+        for a in self.agents:
+            a.policy.train()
+            a.target_policy.train()
+        if device == 'gpu':
+            fn = lambda x: x.cuda()
+        else:
+            fn = lambda x: x.cpu()
+        if not self.pol_dev == device:
+            for a in self.agents:
+                a.policy = fn(a.policy)
+            self.pol_dev = device
+        if not self.critic_dev == device:
+            self.critic = fn(self.critic)
+            self.critic_dev = device
+        if not self.trgt_pol_dev == device:
+            for a in self.agents:
+                a.target_policy = fn(a.target_policy)
+            self.trgt_pol_dev = device
+        if not self.trgt_critic_dev == device:
+            self.target_critic = fn(self.target_critic)
+            self.trgt_critic_dev = device
+
+    def prep_rollouts(self, device='cpu'):
+        for a in self.agents:
+            a.policy.eval()
+        if device == 'gpu':
+            fn = lambda x: x.cuda()
+        else:
+            fn = lambda x: x.cpu()
+        # only need main policy for rollouts
+        if not self.pol_dev == device:
+            for a in self.agents:
+                a.policy = fn(a.policy)
+            self.pol_dev = device
+
+    def save(self, filename):
+        """
+        Save trained parameters of all agents into one file
+        """
+        self.prep_training(device='cpu')  # move parameters to CPU before saving
+        save_dict = {'init_dict': self.init_dict,
+                     'agent_params': [a.get_params() for a in self.agents],
+                     'critic_params': {'critic': self.critic.state_dict(),
+                                       'target_critic': self.target_critic.state_dict(),
+                                       'critic_optimizer': self.critic_optimizer.state_dict()}}
+        torch.save(save_dict, filename)
+
+    @classmethod
+    def init_from_env(cls, env, critic_dim, gamma=0.95, tau=0.01,
+                      pi_lr=0.01, q_lr=0.01,
+                      reward_scale=10.,
+                      pol_hidden_dim=128, critic_hidden_dim=128, attend_heads=4, actor_dim=0,
+                      **kwargs):
+        """
+        Instantiate instance of this class from multi-agent environment
+
+        env: Multi-agent Gym environment
+        gamma: discount factor
+        tau: rate of update for target networks
+        lr: learning rate for networks
+        hidden_dim: number of hidden dimensions for networks
+        """
+        agent_init_params = []
+        sa_size = []
+        for i in range(len(env.all_agents)):
+            sa_size.append((env.env_combined_obs_space, env.env_combined_action_space))
+            agent_init_params.append({'num_in_pol': actor_dim,
+                                      'num_out_pol': env.env_combined_action_space})
+            sa_size.append((env.env_combined_obs_space, env.env_combined_action_space))
+
+        init_dict = {'gamma': gamma, 'tau': tau,
+                     'pi_lr': pi_lr, 'q_lr': q_lr,
+                     'reward_scale': reward_scale,
+                     'pol_hidden_dim': pol_hidden_dim,
+                     'critic_input_dim': critic_dim,
+                     'critic_hidden_dim': critic_hidden_dim,
+                     'attend_heads': attend_heads,
+                     'agent_init_params': agent_init_params,
+                     'sa_size': sa_size}
+        instance = cls(**init_dict)
+        instance.init_dict = init_dict
+        return instance
+
+    @classmethod
+    def init_from_save(cls, filename, load_critic=False):
+        """
+        Instantiate instance of this class from file created by 'save' method
+        """
+        save_dict = torch.load(filename)
+        instance = cls(**save_dict['init_dict'])
+        instance.init_dict = save_dict['init_dict']
+        for a, params in zip(instance.agents, save_dict['agent_params']):
+            a.load_params(params)
+
+        if load_critic:
+            critic_params = save_dict['critic_params']
+            instance.critic.load_state_dict(critic_params['critic'])
+            instance.target_critic.load_state_dict(critic_params['target_critic'])
+            instance.critic_optimizer.load_state_dict(critic_params['critic_optimizer'])
+        return instance
---
+++ basedline_MAAC_ownENV/utils_MAAC/critics.py
@@ -0,0 +1,239 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+from itertools import chain
+
+
+class AttentionCritic(nn.Module):
+    """
+    Attention network, used as critic for all agents. Each agent gets its own
+    observation and action, and can also attend over the other agents' encoded
+    observations and actions.
+    """
+    def __init__(self, sa_sizes, critic_input_dim=None, hidden_dim=32, norm_in=True, attend_heads=1):
+        """
+        Inputs:
+            sa_sizes (list of (int, int)): Size of state and action spaces per
+                                          agent
+            hidden_dim (int): Number of hidden dimensions
+            norm_in (bool): Whether to apply BatchNorm to input
+            attend_heads (int): Number of attention heads to use (use a number
+                                that hidden_dim is divisible by)
+        """
+        super(AttentionCritic, self).__init__()
+        assert (hidden_dim % attend_heads) == 0
+        self.sa_sizes = sa_sizes
+        self.nagents = len(sa_sizes)
+        self.attend_heads = attend_heads
+
+        self.critic_encoders = nn.ModuleList()
+        self.critics = nn.ModuleList()
+
+        self.state_encoders = nn.ModuleList()
+
+        # ------------------------ attention for individual agents --------------------
+        # 5, is the number of agents
+        self.sum_own_fc = nn.Sequential(nn.Linear(critic_input_dim[0]*5, 1024), nn.ReLU())  # may be here can be replaced with another attention mechanism
+        self.sum_grid_fc = nn.Sequential(nn.Linear(critic_input_dim[1]*5, 256), nn.ReLU())
+
+        self.single_own_fc = nn.Sequential(nn.Linear(critic_input_dim[0], 128), nn.ReLU())  # may be here can be replaced with another attention mechanism
+        self.single_grid_fc = nn.Sequential(nn.Linear(critic_input_dim[1], 128), nn.ReLU())
+        self.single_surr = nn.Sequential(nn.Linear(critic_input_dim[2], 128), nn.ReLU())
+        self.single_env_combine = nn.Sequential(nn.Linear(128+128, 64), nn.ReLU())
+
+        self.single_k = nn.Linear(128, 128, bias=False)
+        self.single_q = nn.Linear(128, 128, bias=False)
+        self.single_v = nn.Linear(128, 128, bias=False)
+
+        self.combine_env_fc = nn.Sequential(nn.Linear((5*128)+(5*128), 512), nn.ReLU())
+
+        self.combine_all = nn.Sequential(nn.Linear(512+5 * 5, 512), nn.ReLU(),
+                                         nn.Linear(512, 256), nn.ReLU(), nn.Linear(256, 1))
+
+        # iterate over agents
+        for _ in range(5):  # 5 is the total number of agents
+            encoder = nn.Sequential()
+            encoder.add_module('enc_fc1', nn.Linear(64+2, hidden_dim))
+            encoder.add_module('enc_nl', nn.ReLU())
+            self.critic_encoders.append(encoder)
+
+            critic = nn.Sequential()
+            critic.add_module('critic_fc1', nn.Linear(2 * hidden_dim, hidden_dim))  # this 2, is hard coded from the original code
+            critic.add_module('critic_nl', nn.ReLU())
+            critic.add_module('critic_fc2', nn.Linear(hidden_dim, 2))  # 2 stand for the output action dimension
+            self.critics.append(critic)
+
+            state_encoder = nn.Sequential()
+            state_encoder.add_module('s_enc_fc1', nn.Linear(64, hidden_dim))
+            state_encoder.add_module('s_enc_nl', nn.ReLU())
+            self.state_encoders.append(state_encoder)
+
+        attend_dim = hidden_dim // attend_heads
+        self.key_extractors = nn.ModuleList()
+        self.selector_extractors = nn.ModuleList()
+        self.value_extractors = nn.ModuleList()
+        for i in range(attend_heads):
+            self.key_extractors.append(nn.Linear(hidden_dim, attend_dim, bias=False))
+            self.selector_extractors.append(nn.Linear(hidden_dim, attend_dim, bias=False))
+            self.value_extractors.append(nn.Sequential(nn.Linear(hidden_dim, attend_dim), nn.ReLU()))
+
+        self.shared_modules = [self.key_extractors, self.selector_extractors,
+                               self.value_extractors, self.critic_encoders]
+
+    def shared_parameters(self):
+        """
+        Parameters shared across agents and reward heads
+        """
+        return chain(*[m.parameters() for m in self.shared_modules])
+
+    def scale_shared_grads(self):
+        """
+        Scale gradients for parameters that are shared since they accumulate
+        gradients from the critic loss function multiple times
+        """
+        for p in self.shared_parameters():
+            p.grad.data.mul_(1. / self.nagents)
+
+    def forward(self, inps, agents=None, return_q=True, return_all_q=False,
+                regularize=False, return_attend=False, niter=0):
+        """
+        Inputs:
+            inps (list of PyTorch Matrices): Inputs to each agents' encoder
+                                             (batch of obs + ac)
+            agents (int): indices of agents to return Q for
+            return_q (bool): return Q-value
+            return_all_q (bool): return Q-value for all actions
+            regularize (bool): returns values to add to loss function for
+                               regularization
+            return_attend (bool): return attention weights per agent
+            logger (TensorboardX SummaryWriter): If passed in, important values
+                                                 are logged
+        """
+        if agents is None:
+            agents = range(len(self.critic_encoders))
+        # obtain the state after attention
+        # pre-process, compute attention for every agent based on their surrounding agents
+        encoded_all_agent_state = []
+        for one_agent_batch_own, one_agent_batch_grid, one_agent_batch_surr in zip(*inps[0]):  # automatically loop over 5 times
+            single_grid_out = self.single_grid_fc(one_agent_batch_grid)
+            single_own_out = self.single_own_fc(one_agent_batch_own)
+            single_surr_out = self.single_surr(one_agent_batch_surr)
+            single_q = self.single_q(single_own_out)
+            single_k = self.single_k(single_surr_out)
+            single_v = self.single_v(single_surr_out)
+            mask = one_agent_batch_surr.mean(axis=2, keepdim=True).bool()
+            score = torch.bmm(single_k, single_q.unsqueeze(axis=2))
+            score_mask = score.clone()  # clone操作很必要
+            score_mask[~mask] = float('-inf')  # 不然赋值操作后会无法计算梯度
+            alpha = F.softmax(score_mask / np.sqrt(single_k.size(-1)), dim=1)  # we use dim=1 here because we need to get attention of each sequence in K towards all hidden vector of q in each batch.
+            alpha_mask = alpha.clone()
+            alpha_mask[~mask] = 0
+            v_att = torch.sum(single_v * alpha_mask, axis=1)
+
+            encoded_all_agent_state.append(self.single_env_combine(torch.cat((v_att, single_grid_out), dim=1)))
+
+        states = encoded_all_agent_state
+        actions = [a for a in inps[1]]
+        inps = [torch.cat((s, a), dim=1) for s, a in zip(encoded_all_agent_state, inps[1])]
+        # extract state-action encoding for each agent
+        sa_encodings = []
+        for encoder, inp in zip(self.critic_encoders, inps):
+            one_sa = encoder(inp)
+            sa_encodings.append(one_sa)
+
+        # extract state encoding for each agent that we're returning Q for
+        s_encodings = []
+        for a_i in agents:
+            one_s = self.state_encoders[a_i](states[a_i])
+            s_encodings.append(one_s)
+        # s_encodings = [self.state_encoders[a_i](states[a_i]) for a_i in agents]
+        # extract keys for each head for each agent
+        all_head_keys = []
+        for k_ext in self.key_extractors:
+            each_head_all_agent_key = []
+            for enc in sa_encodings:
+                one_agent_keys = k_ext(enc)
+                each_head_all_agent_key.append(one_agent_keys)
+            all_head_keys.append(each_head_all_agent_key)
+
+        # all_head_keys = [[k_ext(enc) for enc in sa_encodings] for k_ext in self.key_extractors]  # this is the original version. All values the same, thanks to same torch seed.
+
+        # extract sa values for each head for each agent
+        all_head_values = []
+        for v_ext in self.value_extractors:
+            each_head_all_agent_value = []
+            for enc in sa_encodings:
+                one_agent_value = v_ext(enc)
+                each_head_all_agent_value.append(one_agent_value)
+            all_head_values.append(each_head_all_agent_value)
+        # all_head_values = [[v_ext(enc) for enc in sa_encodings] for v_ext in self.value_extractors]  # original
+
+        # extract selectors for each head for each agent that we're returning Q for
+        all_head_selectors = []
+        for sel_ext in self.selector_extractors:
+            each_head_selector = []
+            for i, enc in enumerate(s_encodings):
+                if i in agents:
+                    one_agent_selector = sel_ext(enc)
+                    each_head_selector.append(one_agent_selector)
+            all_head_selectors.append(each_head_selector)
+
+        # all_head_selectors = [[sel_ext(enc) for i, enc in enumerate(s_encodings) if i in agents]for sel_ext in self.selector_extractors]  # original
+
+        other_all_values = [[] for _ in range(len(agents))]
+        all_attend_logits = [[] for _ in range(len(agents))]
+        all_attend_probs = [[] for _ in range(len(agents))]
+        # calculate attention per head
+        for curr_head_keys, curr_head_values, curr_head_selectors in zip(
+                all_head_keys, all_head_values, all_head_selectors):
+            # iterate over agents
+            for i, a_i, selector in zip(range(len(agents)), agents, curr_head_selectors):
+                keys = [k for j, k in enumerate(curr_head_keys) if j != a_i]
+                values = [v for j, v in enumerate(curr_head_values) if j != a_i]
+                # calculate attention across agents
+                attend_logits = torch.matmul(selector.view(selector.shape[0], 1, -1),
+                                             torch.stack(keys).permute(1, 2, 0))
+                # scale dot-products by size of key (from Attention is All You Need)
+                scaled_attend_logits = attend_logits / np.sqrt(keys[0].shape[1])
+                attend_weights = F.softmax(scaled_attend_logits, dim=2)
+                other_values = (torch.stack(values).permute(1, 2, 0) *
+                                attend_weights).sum(dim=2)
+                other_all_values[i].append(other_values)
+                all_attend_logits[i].append(attend_logits)
+                all_attend_probs[i].append(attend_weights)
+        # calculate Q per agent
+        all_rets = []
+        for i, a_i in enumerate(agents):
+            head_entropies = [(-((probs + 1e-8).log() * probs).squeeze().sum(1)
+                               .mean()) for probs in all_attend_probs[i]]
+            agent_rets = []
+            critic_in = torch.cat((s_encodings[i], *other_all_values[i]), dim=1)
+            all_q = self.critics[a_i](critic_in)
+            int_acs = actions[a_i].max(dim=1, keepdim=True)[1]
+            q = all_q.gather(1, int_acs)
+            if return_q:
+                agent_rets.append(q)
+            if return_all_q:
+                agent_rets.append(all_q)
+            if regularize:
+                # regularize magnitude of attention logits
+                attend_mag_reg = 1e-3 * sum((logit**2).mean() for logit in
+                                            all_attend_logits[i])
+                regs = (attend_mag_reg,)
+                agent_rets.append(regs)
+            if return_attend:
+                agent_rets.append(np.array(all_attend_probs[i]))
+            # if logger is not None:
+            #     logger.add_scalars('agent%i/attention' % a_i,
+            #                        dict(('head%i_entropy' % h_i, ent) for h_i, ent
+            #                             in enumerate(head_entropies)),
+            #                        niter)
+            if len(agent_rets) == 1:
+                all_rets.append(agent_rets[0])  # for critic_target without regularization
+            else:
+                all_rets.append(agent_rets)  # for critic_prediction net, with regularization
+        if len(all_rets) == 1:
+            return all_rets[0]
+        else:
+            return all_rets
