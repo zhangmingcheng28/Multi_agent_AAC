6b70b993d88b1c809a36daf16383d9be0ab9816b
zhangmingcheng28 <zhangmingcheng28@hotmail.com>
2024-01-12 12:55:00 +0800

    Update ma_main_randomOD_gru_radar.py

--- MADDPG_ownENV_randomOD_gru_radar/ma_main_randomOD_gru_radar.py
+++ MADDPG_ownENV_randomOD_gru_radar/ma_main_randomOD_gru_radar.py
@@ -199,8 +199,8 @@ def main(args):
         # initialize_excel_file(excel_file_path_time)
         # ------------ end of this portion is to save using excel instead of pickle -----------
 
-    use_wanDB = False
-    # use_wanDB = True
+    # use_wanDB = False
+    use_wanDB = True
     # get_evaluation_status = True  # have figure output
     get_evaluation_status = False  # no figure output, mainly obtain collision rate
     # simply_view_evaluation = True  # don't save gif
@@ -809,7 +809,7 @@ if __name__ == '__main__':
     parser.add_argument('--scenario', default="simple_spread", type=str)
     parser.add_argument('--max_episodes', default=35000, type=int)  # run for a total of 50000 episodes
     parser.add_argument('--algo', default="maddpg", type=str, help="commnet/bicnet/maddpg")
-    parser.add_argument('--mode', default="eval", type=str, help="train/eval")
+    parser.add_argument('--mode', default="train", type=str, help="train/eval")
     parser.add_argument('--episode_length', default=150, type=int)  # maximum play per episode
     parser.add_argument('--memory_length', default=int(1e5), type=int)
     parser.add_argument('--seed', default=777, type=int)  # may choose to use 3407
