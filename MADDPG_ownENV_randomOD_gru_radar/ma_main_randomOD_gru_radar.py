7ade838034d780540b6facb9bfa968b40ff02012
zhangmingcheng28 <zhangmingcheng28@hotmail.com>
2024-01-22 20:43:56 +0800

    try new way of find way point

--- MADDPG_ownENV_randomOD_gru_radar/ma_main_randomOD_gru_radar.py
+++ MADDPG_ownENV_randomOD_gru_radar/ma_main_randomOD_gru_radar.py
@@ -346,8 +346,8 @@ def main(args):
 
     use_wanDB = False
     # use_wanDB = True
-    get_evaluation_status = True  # have figure output
-    # get_evaluation_status = False  # no figure output, mainly obtain collision rate
+    # get_evaluation_status = True  # have figure output
+    get_evaluation_status = False  # no figure output, mainly obtain collision rate
     # simply_view_evaluation = True  # don't save gif
     simply_view_evaluation = False  # save gif
 
@@ -444,8 +444,8 @@ def main(args):
         args.max_episodes = 10  # only evaluate one episode during evaluation mode.
         # args.max_episodes = 5  # only evaluate one episode during evaluation mode.
         # args.max_episodes = 100
-        pre_fix = r'D:\MADDPG_2nd_jp\210124_17_20_59\interval_record_eps'
-        episode_to_check = str(2000)
+        pre_fix = r'D:\MADDPG_2nd_jp'
+        episode_to_check = str(8000)
         load_filepath_0 = pre_fix + '\episode_' + episode_to_check + '_agent_0actor_net.pth'
         load_filepath_1 = pre_fix + '\episode_' + episode_to_check + '_agent_1actor_net.pth'
         load_filepath_2 = pre_fix + '\episode_' + episode_to_check + '_agent_2actor_net.pth'
